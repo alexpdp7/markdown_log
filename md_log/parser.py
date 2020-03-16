533a63db9a973e7af6a13f56ed0531d12d77ea4d
alex <alex@pdp7.net>
2020-03-16 11:22:05 +0100

    Refactor out parser module
    * Slightly cleaner API

---
+++ md_log/parser.py
@@ -0,0 +1,81 @@
+import dataclasses
+import datetime
+import itertools
+import typing
+
+import panflute as pf
+
+
+@dataclasses.dataclass
+class Period:
+    begin: datetime.datetime
+    end: datetime.datetime
+    task_hierarchies: typing.List[typing.List[str]] = dataclasses.field(
+        default_factory=list
+    )
+    items: typing.List[pf.ListItem] = dataclasses.field(default_factory=list)
+
+
+class Walker:
+    def __init__(self):
+        self.period = None
+        self.periods = []
+        self.current_day = None
+
+    def parse_file(self, f):
+        doc = pf.convert_text(f.read(), standalone=True)
+        doc.walk(self.action)
+
+    def action(self, elem, doc):
+        if isinstance(elem, pf.Header):
+            if elem.level == 1:
+                day_text = elem.content[0].text
+                try:
+                    self.current_day = datetime.date.fromisoformat(day_text)
+                except ValueError:
+                    print(f"Ignoring header for day {day_text}")
+                    self.current_day = None
+            if elem.level == 2:
+                period_text = elem.content[0].text
+                try:
+                    begin_time, end_time = map(
+                        datetime.time.fromisoformat, period_text.split("-")
+                    )
+                    begin = datetime.datetime.combine(self.current_day, begin_time)
+                    end = datetime.datetime.combine(self.current_day, end_time)
+                    self.period = Period(begin=begin, end=end)
+                    self.periods.append(self.period)
+                except ValueError:
+                    print(f"Ignoring header for period {period_text}")
+                    self.period = None
+            if elem.level == 3:
+                # first, partition on '/'s
+                def is_slash(e):
+                    return isinstance(e, pf.Str) and e.text == "/"
+
+                parts = [
+                    list(content)
+                    for group, content in itertools.groupby(elem.content, is_slash)
+                    if not group
+                ]
+
+                def elem_to_text(e):
+                    if isinstance(e, pf.Space):
+                        return " "
+                    return e.text
+
+                def elems_to_text(e):
+                    return " ".join(map(elem_to_text, e)).strip()
+
+                task_hierarchy = list(map(elems_to_text, parts))
+                self.period.task_hierarchies.append(task_hierarchy)
+        else:
+            if self.period and isinstance(elem, pf.ListItem):
+                self.period.items.append(elem)
+
+
+def parse(files):
+    walker = Walker()
+    for file in files:
+        walker.parse_file(file)
+    return walker.periods
---
+++ tests/test_parser.py
@@ -0,0 +1,30 @@
+import datetime
+import io
+
+import panflute
+
+from md_log import parser
+
+
+def test_basic():
+    log = """
+# 2019-12-03
+
+## 09:00-10:00
+
+### This / is / an / activity
+
+### Another / thing
+
+* I did some stuff
+"""
+    periods = parser.parse([io.StringIO(log)])
+    assert len(periods) == 1
+    assert periods[0].begin == datetime.datetime(2019, 12, 3, 9, 0)
+    assert periods[0].end == datetime.datetime(2019, 12, 3, 10, 0)
+    assert periods[0].task_hierarchies == [
+        ["This", "is", "an", "activity"],
+        ["Another", "thing"],
+    ]
+    assert len(periods[0].items) == 1
+    assert panflute.stringify(periods[0].items[0]) == "I did some stuff"
