2645567dcc08a1042bcaa62a59e84f3bf98d1b15
alex <alex@pdp7.net>
2020-03-31 14:32:08 +0200

    Make daily-target report prettier
    * Major refactor
    * Add tabulate
    * Update docs; table is clearer now so should require less explanation.
      Also tweak example so it shows more cases and is clearer.

--- md_log/daily_targets.py
+++ md_log/daily_targets.py
@@ -1,6 +1,8 @@
 import argparse
-import collections
+import dataclasses
 import datetime
+
+import tabulate
 
 from md_log import parser
 
@@ -43,37 +45,91 @@ def _matches_filter(task_hierarchies, filter):
     return any([_hierarchy_matches_filter(h, filter) for h in task_hierarchies])
 
 
-def _make_report(periods, filter, target_hours):
-    date_hours = collections.defaultdict(datetime.timedelta)
-    for period in periods:
-        if _matches_filter(period.task_hierarchies, filter):
-            date_hours[period.begin.date()] += period.end - period.begin
-
-    target_diff = datetime.timedelta()
-    date_target = dict()
-    for day in sorted(date_hours.keys(), reverse=True):
-        target_diff += target_hours
-        target_diff -= date_hours[day]
-        date_target[day] = target_diff
-    return date_hours, date_target
+def _sign(timedelta):
+    if timedelta.total_seconds() > 0:
+        return "+"
+    if timedelta.total_seconds() < 0:
+        return "-"
+    return "="
 
 
-def _to_str_target_diff(timedelta):
-    type = (
-        "exact"
-        if timedelta == datetime.timedelta()
-        else "too little"
-        if timedelta.total_seconds() > 0
-        else "too much"
+def _format(timedelta):
+    return f"{_sign(timedelta)} {abs(timedelta)}"
+
+
+class Report:
+    def __init__(self, target, filter):
+        self.target = target
+        self.filter = filter
+        self.days = dict()
+
+    def _make_day(self, date):
+        return Day(date=date, report=self)
+
+    def add_period(self, period):
+        if _matches_filter(period.task_hierarchies, self.filter):
+            date = period.begin.date()
+            day = self.days.get(date, self._make_day(date))
+            day.matching_time += period.end - period.begin
+            self.days[date] = day
+
+    def update_running_difference_to_target(self):
+        target_diff = datetime.timedelta()
+        for day in sorted(self.days.keys(), reverse=True):
+            target_diff -= self.target
+            target_diff += self.days[day].matching_time
+            self.days[day].running_difference_to_target = target_diff
+
+    def get_table(self):
+        return (
+            ["Date", "Matching", "", "Target", "", "Delta", "", "Running"],
+            [day.as_row() for _, day in sorted(self.days.items())],
+        )
+
+
+@dataclasses.dataclass
+class Day:
+    date: datetime.date
+    report: Report
+    running_difference_to_target: datetime.timedelta = dataclasses.field(
+        default_factory=lambda: None,
     )
-    return f"{type} {abs(timedelta)}"
+    matching_time: datetime.timedelta = dataclasses.field(
+        default_factory=datetime.timedelta,
+    )
+
+    @property
+    def target(self):
+        return self.report.target
+
+    @property
+    def difference_to_target(self):
+        return self.matching_time - self.target
+
+    def as_row(self):
+        return [
+            self.date,
+            self.matching_time,
+            "-",
+            self.target,
+            "=",
+            _format(self.difference_to_target),
+            "+ \u2198 = ",
+            _format(self.running_difference_to_target),
+        ]
+
+
+def _make_report(periods, filter, target):
+    report = Report(target, filter)
+    for period in periods:
+        report.add_period(period)
+    report.update_running_difference_to_target()
+    return report
 
 
 def daily_targets(args):
     periods = parser.parse(args.logfiles)
     filter = args.filter.split("/") if args.filter else None
-    date_hours, date_target = _make_report(
-        periods, filter, datetime.timedelta(hours=args.target_hours)
-    )
-    for day in sorted(date_hours.keys()):
-        print(day, date_hours[day], _to_str_target_diff(date_target[day]))
+    report = _make_report(periods, filter, datetime.timedelta(hours=args.target_hours))
+    headers, table = report.get_table()
+    print(tabulate.tabulate(table, headers, colalign=("right",) * len(headers)))
