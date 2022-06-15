68dce20e148020cafe17a62867658cdd18b97968
alex <alex@pdp7.net>
2022-06-15 12:49:07 +0200

    Adjust xbar formatting
    * Move refresh to the top so it's always visible
    * Compact things

--- md_log/xbar.py
+++ md_log/xbar.py
@@ -14,26 +14,32 @@ Acts as an xbar/Argos plugin
 def xbar(args):
     print("Log")
     print("---")
+
+    print("refresh | refresh=true")
+
     filter = args.filter.split("/") if args.filter else None
     headers, table = daily_targets.create_daily_targets_report(
         [args.logfile], filter, args.target_hours, True
     )
     table = tabulate.tabulate(table, headers, stralign="right", tablefmt="rst")
     table = table.replace("\n", " | font=mono trim=false\n")
+    table += " | font=mono trim=false"
     print(table)
-    print()
+
     args.logfile.seek(0)
     found_today = False
     stopped = False
+
     for line in args.logfile.readlines():
         line = line.strip()
         if datetime.date.today().strftime("# %Y-%m-%d") == line:
             found_today = True
         if "STOP_XBAR" in line:
             stopped = True
-        if found_today and not stopped:
-            print(line + "| font=mono")
-    print("refresh | refresh=true")
+        if found_today and not stopped and line:
+            if line.startswith("## "):
+                print()
+            print(line)
 
 
 def make_parser(subparsers):
